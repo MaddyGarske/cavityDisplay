c115fc0ca4693b01726fcdaa66ebb7c6421bf4b0
mwgarske <mwgarske@slac.stanford.edu>
2021-04-03 13:04:04 -0700

    fixed typo of _init_ instead of __init__ now launches correctly & cleaned up a bit

--- single_cavity.py
+++ single_cavity.py
@@ -1,21 +1,15 @@
-from PyQt5 import QtGui
-#import meme.archive
-#import epics
-#from datetime import datetime
-#import time
-#from pytz import utc, timezone
-#from random import randint
-#from sys import argv, exit
 from pydm import Display
-#from PyQt5.QtCore import QDateTime, Qt #QTimer, QObject, pyqtSignal, QDate
-#from urllib2 import urlopen
-from json import load
+from PyQt5 import QtGui, QtCore, QtWidgets
+import epics
+from epics import caget, caput
 
 class Cavity(Display):
-
-#below four lines must be present in any pydm GUI
-	def __init__(self, parent=None, args=None):
-		super(Cavity, self)._init_(parent=parent, args=args)	
-		
+	#must be present in any pydm GUI
 	def	ui_filename(self):
 		return 'single_cavity.ui'
+
+	#must be present in any pydm GUI
+	def __init__(self, parent=None, args=None):
+		super(Cavity, self).__init__(parent=parent, args=args)	
+		
+
